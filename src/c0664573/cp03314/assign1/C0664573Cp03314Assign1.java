f34e6a65d853bd66483276e7cc42300fe586f8b9
kalaipriya <c0664573@mylambton.ca>
2015-09-16 16:28:58 -0400

    finished assignment

--- src/c0664573/cp03314/assign1/C0664573Cp03314Assign1.java
+++ src/c0664573/cp03314/assign1/C0664573Cp03314Assign1.java
@@ -17,6 +17,17 @@ public class C0664573Cp03314Assign1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
+        /****************************************************
+         *  Assignment 1
+         * Job posting
+         * Senior Java Developer - TekHunter - Greater Toronto Area
+         * JDBC - Java Database Connectivity - http://www.oracle.com/technetwork/java/javase/jdbc/index.html
+         * JSP - Java Server Pages - https://en.wikipedia.org/wiki/JavaServer_Pages
+         * AJAX - Asynchronous Java Script and XML - https://en.wikipedia.org/wiki/Ajax_(programming)
+         * Toronto Salary - $91,409
+         * Vancouver Salary - $100,827
+         * Waterloo,OH Salary - $88,195 
+         */
     }
     
 }
